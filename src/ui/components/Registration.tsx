af313b8fd027c983070a4784d1dfec99ead4a264
AKSUMRUS <shimorhershow@gmail.com>
2023-10-15 06:15:02 +0300

    feat: IT WORKS. Added simple example

--- src/ui/components/Registration.tsx
+++ src/ui/components/Registration.tsx
@@ -3,41 +3,12 @@ import {autoInjectable, container, inject, injectable, singleton} from "tsyringe
 import React, {useState} from "react";
 import {ViewModel} from "@yoskutik/react-vvm";
 
-@singleton()
-class Person {
-    text = 'hello'
-    constructor() {
-    }
-}
-
-@singleton()
-class Person2{
-    constructor(public person: Person) {
-
-    }
-}
-
-@singleton()
-class Child1 {}
-
-@singleton()
-class Child2 {}
-
-@singleton()
-class Parent {
-    constructor(public readonly child1: Child1, public readonly child2: Child2) {}
-}
-
-const parent = container.resolve(Parent);
-
-
 const Registration = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const handlePasswordChange = (value: string) => {
         setPassword(value);
     };
-    // const person = container.resolve(Person2)
 
     const handleEmailChange = (value: string) => {
         setEmail(value);
@@ -46,7 +17,7 @@ const Registration = () => {
     return (
         <div>
             <RegistrationField
-                email={"person.person.text"}
+                email={email}
                 handleEmailChange={handleEmailChange}
                 password={password}
                 handlePasswordChange={handlePasswordChange}
