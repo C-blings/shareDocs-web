609359a7891fed0c0f77cdb588b6c21093144da2
AKSUMRUS <shimorhershow@gmail.com>
2023-10-15 04:19:20 +0300

    idk

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,14 +1,13 @@
-// import "reflect-metadata";
 import React from 'react';
 import './App.css';
 import {computed, makeObservable} from "mobx";
 import {BasicViewModel} from "./ui/viewmodels/BasicViewModel";
-import {configure, view, ViewModel} from "@yoskutik/react-vvm";
+import {view, ViewModel} from "@yoskutik/react-vvm";
 import {AppStore} from "./data/repository/AppStore";
-import {container, inject, injectable} from "tsyringe";
+import {injectable} from "tsyringe";
 
 @injectable()
-class AppViewModel extends ViewModel {
+export class AppViewModel extends ViewModel {
     @computed get text(): string {
         return this.app.greetingText;
     }
@@ -24,10 +23,10 @@ class AppViewModel extends ViewModel {
 }
 
 const App = view(AppViewModel)(({viewModel}) => (
-        <div className="App">
-            <h1>{viewModel.text}</h1>
-            <button onClick={viewModel.onChangeGreeting}>Click me</button>
-        </div>
-    ));
+    <div className="App">
+        <h1>{viewModel.text}</h1>
+        <button onClick={viewModel.onChangeGreeting}>Click me</button>
+    </div>
+));
 
 export default App;
